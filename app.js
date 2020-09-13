94422ec1ac458da0d1c91f6fe4d0fcee5ca32b43
Viandika Fauzi <vian.uzi@gmail.com>
2020-09-13 09:06:11 +0700

    CRUD completed (i think)

---
+++ app.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const db = require("./models");
+
+const app = express();
+PORT = process.env.PORT || "5000";
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/api/user", require("./routes/api/user-route"));
+app.use("/api/profile", require("./routes/api/profile-route"));
+app.use("/api/history", require("./routes/api/history-route"));
+app.get("/", (req, res) => {
+  res.send("default page");
+});
+
+db.sequelize.sync().then(() => {
+  console.log("Database is connected...");
+  app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}...`);
+  });
+});
---
+++ routes/api/user-route.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const router = express.Router();
+const db = require("./../../models");
+const { v4: uuidv4 } = require("uuid");
+const { route } = require("./profile-route");
+
+router.get("/", (req, res) => {
+  db.User.findAll({
+    include: [db.Profile, db.History],
+  })
+    .then((allUser) => res.send(allUser))
+    .catch((err) => res.status(400).json({ msg: err.message }));
+});
+
+router.get("/:id", (req, res) => {
+  db.User.findOne({
+    where: {
+      id: req.params.id,
+      include: [db.Profile, db.History],
+    },
+  })
+    .then((selectedUser) => {
+      if (!selectedUser) {
+        res.status(404).json({ msg: "User does not exist!" });
+      }
+
+      res.json(selectedUser);
+    })
+    .catch((err) => res.status(400).json({ msg: err.message }));
+});
+
+router.post("/", (req, res) => {
+  db.User.create({
+    id: uuidv4(),
+    username: req.body.username,
+    password: req.body.password,
+  })
+    .then((newUser) => res.send(newUser))
+    .catch((err) => res.status(400).json({ msg: err.message }));
+});
+
+router.put("/:id", (req, res) => {
+  db.User.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((modifiedUser) => {
+      if (!modifiedUser) {
+        res.status(404).json({ msg: "No user defined!" });
+      }
+
+      if (req.body.username) {
+        modifiedUser.username = req.body.username;
+      }
+      if (req.body.password) {
+        modifiedUser.password = req.body.password;
+      }
+      modifiedUser
+        .save()
+        .then(res.send(modifiedUser))
+        .catch((err) => res.status(501).json({ msg: "Error updating user!" }));
+    })
+    .catch((err) => res.status(400).json({ msg: err.message }));
+});
+
+router.delete("/:id", (req, res) => {
+  db.User.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedUser) => {
+      if (!deletedUser) {
+        res.status(404).json({ msg: "User does not exist!" });
+      }
+
+      // res.json(deletedUser);
+      deletedUser
+        .destroy()
+        .then(res.json({ msg: "User successfully deleted" }))
+        .catch((e) => res.status(400).json({ msg: e.message }));
+    })
+    .catch((err) => res.status(400).json({ msg: err.message }));
+});
+
+module.exports = router;
